28b76f3362fa7954c5bed7e3f4fedfabbfb5ab14
Eshan Chatterjee <eshan2001221@gmail.com>
2022-09-17 23:20:07 +0530

    gitignore upadte

--- scripts/build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rustup target add wasm32-unknown-unknown
-cargo install trunk
-trunk build --release
-trunk serve
--- setup.sh
+++
@@ -1,4 +0,0 @@
-$ curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
-rustup target add wasm32-unknown-unknown
-cargo install trunk
-trunk build --release
